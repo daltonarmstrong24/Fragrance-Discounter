1c02a16ba50ad3f825600fe6b47d4f9a35dab709
daltonarmstrong24 <dalton.armstrong.24@cnu.edu>
2026-04-15 22:57:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ with st.sidebar:
     
     try:
         # Using .select(count='exact') is more stable than .query("count")
-        brand_res = conn.table("Brands").select("*", count="exact").execute()
-        frag_res = conn.table("Fragrances").select("*", count="exact").execute()
+        brand_res = conn.table("brands").select("*", count="exact").execute()
+        frag_res = conn.table("fragrances").select("*", count="exact").execute()
         
         st.metric("Total Brands", len(brand_res.data))
         st.metric("Unique Fragrances", len(frag_res.data))
@@ -30,7 +30,7 @@ with st.sidebar:
     st.subheader("Filter by Brand")
     
     # Get brands for dropdown
-    brand_data = conn.table("Brands").select("brandID, brand_name").execute()
+    brand_data = conn.table("brands").select("brandID, brand_name").execute()
     brand_list = {item['brand_name']: item['brandID'] for item in brand_data.data}
     selected_brand = st.selectbox("Select a Brand", options=["All"] + list(brand_list.keys()))
 
@@ -42,7 +42,7 @@ search_query = st.text_input("Search for a fragrance name...")
 try:
     # Use the table name exactly as it appears in your Supabase dashboard
     # If your table is 'FragranceVariants' with a capital V, change it here:
-    builder = conn.table("FragranceVariants").select("""
+    builder = conn.table("fragrancevariants").select("""
         price, 
         fragSize, 
         stockAmount,
@@ -66,11 +66,11 @@ try:
         for item in results.data:
             if item.get('Fragrances'):
                 clean_data.append({
-                    "Brand": item['Fragrances']['Brands']['brand_name'],
-                    "Fragrance": item['Fragrances']['frag_name'],
-                    "Size": item['fragSize'],
-                    "Price": f"${item['price']}",
-                    "Stock": item['stockAmount']
+                    "brand": item['fragrances']['brands']['brand_name'],
+                    "fragrance": item['fragrances']['frag_name'],
+                    "size": item['fragSize'],
+                    "price": f"${item['price']}",
+                    "stock": item['stockAmount']
                 })
         st.dataframe(clean_data, use_container_width=True)
     else:
